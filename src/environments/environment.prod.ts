0c92b89d73e818a6d8f673557d8cf283ec4c5286
yararosasilva <yararosadasilva19@gmail.com>
2025-05-30 16:20:29 -0300

    up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://blogify-cf4p.onrender.com/api'
+  apiUrl: 'https://blogify-cf4p.onrender.com/api',
+  publicEndpoints: [
+    '/auth/login',
+    '/auth/register',
+    '/usuarios/cadastrar'
+  ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://blogify-cf4p.onrender.com/api',
-  publicEndpoints: [
-    '/auth/login',
-    '/auth/register',
-    '/usuarios/cadastrar'
-  ]
+  apiUrl: 'http://localhost:8080/api'
 };
